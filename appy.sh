c2892d0f98546c78394e18ba323205708e12122c
Giovanni2442 <gumrtl@live,com>
2024-05-01 18:02:56 -0600

    add

--- appy.sh
+++ appy.sh
@@ -3,15 +3,15 @@
 #bahc script   : Automatiza instrucciónes en consola desde un scripting. Esto nos ayuda para la creación
 #                del contenedor de manera automatica y rapida
 
-mkdir tempdir  #Crea una carpeta "tempdir"
-mkdir tempdir/templates
-mkdir tempdir/static
+#mkdir tempdir  #Crea una carpeta "tempdir"
+#mkdir tempdir/templates
+#mkdir tempdir/static
 
-cp app.py tempdir                         #Copiar el archivo "app.py" a  carpeta "tempdir"  
-cp requirements.txt tempdir             
-cp Dockerfile tempdir
-cp -r templates/* tempdir/templates       #mover toda una carpeta a otra
-cp -r static/* tempdir/static
+#cp app.py tempdir                         #Copiar el archivo "app.py" a  carpeta "tempdir"  
+#cp requirements.txt tempdir             
+#cp Dockerfile tempdir
+#cp -r templates/* tempdir/templates       #mover toda una carpeta a otra
+#cp -r static/* tempdir/static
 
 cd tempdir                                # acceder a la carpeta tempdir
 
